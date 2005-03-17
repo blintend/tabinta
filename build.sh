52583c5c9481bbe68a33e4944ae504d954c4baa1
balint <balint@localhost>
2005-03-17 23:05:27 +0000

    Added files.

---
+++ build.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+set -e  # terminate on error
+ROOT="`pwd`"
+
+# constants
+
+SRC_DIR="$ROOT"/src
+RELEASE_DIR="$ROOT"/downloads
+
+BUILD_DIR="$ROOT"/_build
+DIST_DIR="$ROOT"/_dist
+
+# functions
+
+build() {
+    [ ! -e "$BUILD_DIR" ] || rm -Rf "$BUILD_DIR"
+    cp -a "$SRC_DIR" "$BUILD_DIR"
+    rm -Rf "$BUILD_DIR"/chrome/*
+    ( cd "$SRC_DIR"/chrome && zip -qr "$BUILD_DIR"/chrome/tabinta.jar * )
+}
+
+xpi() {
+    [ ! -e "$DIST_DIR" ] || rm -Rf "$DIST_DIR"
+    mkdir "$DIST_DIR"
+    cd "$BUILD_DIR" && zip -qr "$DIST_DIR"/tabinta.xpi *
+}
+
+# script code
+
+build
+xpi
