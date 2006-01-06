6905938632579906aefe0d09c5fd517d0227a104
balint <balint@localhost>
2006-01-06 19:53:37 +0000

    Added third-party counter

---
+++ www/protected/counter.php
@@ -0,0 +1,12 @@
+<!-- Start of StatCounter Code -->
+<script type="text/javascript" language="javascript">
+<!-- 
+var sc_project=1176553; 
+var sc_invisible=1; 
+var sc_partition=10; 
+var sc_security="354d1644"; 
+//-->
+</script>
+
+<script type="text/javascript" language="javascript" src="http://www.statcounter.com/counter/counter.js"></script><noscript><a href="http://www.statcounter.com/" target="_blank"><img  src="http://c11.statcounter.com/counter.php?sc_project=1176553&amp;java=0&amp;security=354d1644&amp;invisible=1" alt="web page hit counter" border="0"></a> </noscript>
+<!-- End of StatCounter Code -->
