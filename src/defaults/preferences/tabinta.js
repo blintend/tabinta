ca74a8b730187172c3fd6749a1167521071e4963
balint <balint@localhost>
2008-03-13 21:53:17 +0000

    Gmail 'to' field exception: for https page too

--- src/defaults/preferences/tabinta.js
+++ src/defaults/preferences/tabinta.js
@@ -1,4 +1,4 @@
 pref("tabinta.active", true);
 pref("tabinta.key", "keyCode:9");
-pref("tabinta.filter.0.href", "http://mail.google.com/mail/");
+pref("tabinta.filter.0.href", "https?://mail.google.com/mail/");
 pref("tabinta.filter.0.ctlname", "to");
