a652d4e37025e62cf684cd21f9de62cd6928e296
balint <balint@localhost>
2010-11-27 20:45:28 +0000

    Apply js module pattern

--- src/chrome/content/tabinta/tabintaOverlay.js
+++ src/chrome/content/tabinta/tabintaOverlay.js
@@ -1,153 +1,153 @@
-var common = {};
-Components.utils.import("resource://tabinta/modules/common.jsm", common);
-
-var tabinta = {
+var tabinta = (function(undefined) {
 
     /* Initialization */
     
-    prefb: undefined,   // preferences branch
+    var common = {};
+    Components.utils.import("resource://tabinta/modules/common.jsm", common);
+
+    const ROWS_IGNORE = 1;
+
+    var prefb = undefined;   // preferences branch
     
-    keyCode: undefined,
-    charCode: undefined,
-    shiftKey: undefined,
-    ctrlKey: undefined,
-    altKey: undefined,
-    metaKey: undefined,
+    var keyCode = undefined;
+    var charCode = undefined;
+    var shiftKey = undefined;
+    var ctrlKey = undefined;
+    var altKey = undefined;
+    var metaKey = undefined;
 
-    ROWS_IGNORE: 1,
-
-    init: function() {
-        tabinta.prefb = common.getPrefBranch("tabinta.");
-        tabinta.prefb.addObserver("active", tabinta.activeObserver, false);
-        tabinta.prefb.addObserver("key", tabinta.keyObserver, false);
-        tabinta.syncActive();
-        tabinta.syncKey();
+    function init() {
+        prefb = common.getPrefBranch("tabinta.");
+        prefb.addObserver("active", activeObserver, false);
+        prefb.addObserver("key", keyObserver, false);
+        syncActive();
+        syncKey();
         // context menu setup:
         var menu = document.getElementById("contentAreaContextMenu");
-        menu.addEventListener("popupshowing", tabinta.contextShowing, false);
-    },
+        menu.addEventListener("popupshowing", contextShowing, false);
+    }
     
     /* Context menu handling */
 
-    contextShowing: function() {
-      var inta = gContextMenu.onTextInput
-          && gContextMenu.target.localName.toUpperCase() == "TEXTAREA";
-      gContextMenu.showItem("context-tabinta", inta);
-      gContextMenu.showItem("context-sep-tabinta", inta);
-    },
+    function contextShowing() {
+        var inta = gContextMenu.onTextInput
+            && gContextMenu.target.localName.toUpperCase() == "TEXTAREA";
+        gContextMenu.showItem("context-tabinta", inta);
+        gContextMenu.showItem("context-sep-tabinta", inta);
+    }
     
-    activeToggle: function() {
-      tabinta.setActive(!tabinta.isActive());
-    },
+    function activeToggle() {
+        setActive(!isActive());
+    }
     
     /* Active/passive state handling */
     
-    activeObserver: {
+    var activeObserver = {
         observe: function(subject, topic, data) {
-            tabinta.syncActive();
+            syncActive();
         }
-    },
+    };
     
-    isActive: function() {
-        return tabinta.prefb.getBoolPref("active");
-    },
+    function isActive() {
+        return prefb.getBoolPref("active");
+    }
 
-    setActive: function(active) {
-        tabinta.prefb.setBoolPref("active", active);
-    },
+    function setActive(active) {
+        prefb.setBoolPref("active", active);
+    }
 
-    syncActive: function() {
-        var active = tabinta.isActive();
+    function syncActive() {
+        var active = isActive();
         var appcontent = document.getElementById("appcontent");
         if (active) {
-            appcontent.addEventListener("keypress", tabinta.onKeyPress, true);
+            appcontent.addEventListener("keypress", onKeyPress, true);
         } else {
-            appcontent.removeEventListener("keypress", tabinta.onKeyPress, true);
+            appcontent.removeEventListener("keypress", onKeyPress, true);
         }
         document.getElementById("context-tabinta")
             .setAttribute("checked", active);
-    },
+    }
 
     /* Key definition */
     
-    keyObserver: {
+    var keyObserver = {
         observe: function(subject, topic, data) {
-            tabinta.syncKey();
+            syncKey();
         }
-    },
+    };
 
-    getKeyStr: function() {
-        return tabinta.prefb.getCharPref("key");
-    },
+    function getKeyStr() {
+        return prefb.getCharPref("key");
+    }
 
     /* e.g. _extractProp("ab:c, de:f, gh:i", "de") -> "f" */
-    _extractProp: function(str, prop, dflt) {
+    function _extractProp(str, prop, dflt) {
         var re = new RegExp("\\b" + prop + "\\s*:\\s*(\\w+)\\s*(,|$)");
         var match = re.exec(str);
         return match ? match[1] : dflt;
-    },
+    }
     
-    syncKey: function() {
-        var keyStr = tabinta.getKeyStr();
-        tabinta.keyCode = tabinta._extractProp(keyStr, "keyCode", 0);
-        tabinta.charCode = tabinta._extractProp(keyStr, "charCode", 0);
-        tabinta.shiftKey = tabinta._extractProp(keyStr, "shiftKey", "false") == "true";
-        tabinta.ctrlKey = tabinta._extractProp(keyStr, "ctrlKey", "false") == "true";
-        tabinta.altKey = tabinta._extractProp(keyStr, "altKey", "false") == "true";
-        tabinta.metaKey = tabinta._extractProp(keyStr, "metaKey", "false") == "true";
-    },
+    function syncKey() {
+        var keyStr = getKeyStr();
+        keyCode = _extractProp(keyStr, "keyCode", 0);
+        charCode = _extractProp(keyStr, "charCode", 0);
+        shiftKey = _extractProp(keyStr, "shiftKey", "false") == "true";
+        ctrlKey = _extractProp(keyStr, "ctrlKey", "false") == "true";
+        altKey = _extractProp(keyStr, "altKey", "false") == "true";
+        metaKey = _extractProp(keyStr, "metaKey", "false") == "true";
+    }
     
     /* Tabinta functionality */
     
-    onKeyPress: function(event) {
-        if (event.keyCode==tabinta.keyCode
-                && event.charCode==tabinta.charCode
+    function onKeyPress(event) {
+        if (event.keyCode==keyCode
+                && event.charCode==charCode
                 && event.originalTarget.nodeName.toUpperCase()=="TEXTAREA"
-                && event.shiftKey==tabinta.shiftKey
-                && event.ctrlKey==tabinta.ctrlKey
-                && event.altKey==tabinta.altKey
-                && event.metaKey==tabinta.metaKey
-                && tabinta.rowLimit(event)
-                && tabinta.filter(event)) {
-            tabinta.insertTab(event.originalTarget);
+                && event.shiftKey==shiftKey
+                && event.ctrlKey==ctrlKey
+                && event.altKey==altKey
+                && event.metaKey==metaKey
+                && rowLimit(event)
+                && filter(event)) {
+            insertTab(event.originalTarget);
             event.preventDefault();
             event.stopPropagation();    // aggressive; needed for vbulletin; see http://www.linuxquestions.org/questions/lq-suggestions-and-feedback-7/how-does-one-enter-a-tab-character-into-a-post-834210/
         }
-    },
+    }
 
-    insertTab: function(element) {
+    function insertTab(element) {
         var text;
-        if (tabinta.isHard()) {
+        if (isHard()) {
             text = "\t";
 	} else {
-            var tabWidth = tabinta.getTabWidth();
-            var tabPos = tabinta.getPosOffset(element);
-            text = tabinta.spaces(tabWidth - (tabPos % tabWidth));
+            var tabWidth = getTabWidth();
+            var tabPos = getPosOffset(element);
+            text = spaces(tabWidth - (tabPos % tabWidth));
 	}
         common.insertText(element, text);
-    },
+    }
     
     /* Spaces-as-tab */
 
-    isHard: function() {
-        return tabinta.prefb.getBoolPref("hard");
-    },
+    function isHard() {
+        return prefb.getBoolPref("hard");
+    }
 
-    getTabWidth: function() {
-        return tabinta.prefb.getIntPref("tab_width");
-    },
+    function getTabWidth() {
+        return prefb.getIntPref("tab_width");
+    }
 
     // return offset relative to the start of the line or the last tab character
-    getPosOffset: function(element) {
+    function getPosOffset(element) {
         var value = element.value;
         var pos = element.selectionStart;
         for (var i=pos-1; i>=0; --i) {
             if ("\n\r\t".indexOf(value.charAt(i)) >= 0) break;
         }
         return pos-1-i;
-    },
+    }
 
-    spaces: function(n) {
+    function spaces(n) {
         var SP8 = "        ";
         var sp = "";
         while (n > 8) {
@@ -155,39 +155,44 @@ var tabinta = {
             n -= 8;
         }
         return sp + SP8.substring(0, n);
-    },
+    }
 
     /* Exclude unwanted fields */
 
-    filter: function(event) {
-        for (var i=0; tabinta.hasExclude(i); ++i) {
-            if (tabinta.isExcluded(event, i)) return false;
+    function filter(event) {
+        for (var i=0; hasExclude(i); ++i) {
+            if (isExcluded(event, i)) return false;
         }
         return true;
-    },
+    }
 
-    hasExclude: function(idx) {
+    function hasExclude(idx) {
         try {
-            tabinta.prefb.getCharPref("filter."+idx+".href");
+            prefb.getCharPref("filter."+idx+".href");
         } catch(e) { // we assume that the pref does not exist
             return false;
         }
         return true;
-    },
+    }
 
-    isExcluded: function(event, idx) {
-        if (new RegExp(tabinta.prefb.getCharPref("filter."+idx+".href")).test(content.location.href)
-                && tabinta.prefb.getCharPref("filter."+idx+".ctlname") == event.originalTarget.name) {
+    function isExcluded(event, idx) {
+        if (new RegExp(prefb.getCharPref("filter."+idx+".href")).test(content.location.href)
+                && prefb.getCharPref("filter."+idx+".ctlname") == event.originalTarget.name) {
             return true;
         } else {
             return false;
         }
-    },
+    }
 
-    rowLimit: function(event) {
+    function rowLimit(event) {
         var ctl = event.originalTarget;
-        if (ctl.rows>0 && ctl.rows<=tabinta.ROWS_IGNORE) return false;
+        if (ctl.rows>0 && ctl.rows<=ROWS_IGNORE) return false;
         return true;
     }
 
-}
+    return {
+        init: init,
+        activeToggle: activeToggle
+    };
+
+})();
